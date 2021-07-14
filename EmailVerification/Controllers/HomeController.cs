ef2b2a3984d2d3e47da69ccad96efdcea844d785
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-07-14 13:14:22 +0700

    Add project files.

---
+++ EmailVerification/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailVerification.Controllers {
+    public class HomeController : Controller {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+        [Authorize]
+        [Route("[action]")]
+        public IActionResult Secret() {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult Login() {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult Reg() {
+            return View();
+        }
+        [Route("[action]")]
+        public async Task<IActionResult> LogOutAsync() {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(string username, string password) {
+            var user = await _userManager.FindByNameAsync(username);
+
+            await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+            return RedirectToAction("Secret");
+        }
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> RegAsync(string username, string password) {
+
+            await _userManager.CreateAsync(new IdentityUser { UserName = username }, password);
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ EmailVerification/Model/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailVerification.Model {
+    public class AuthDbContext : IdentityDbContext {
+        public AuthDbContext(DbContextOptions options) : base(options) {
+        }
+    }
+}
